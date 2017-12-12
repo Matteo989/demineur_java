c3986305ad8a4cd80a0737453fe5f342002f8fbe
BENEZIT Luca <luca.benezit@etu.univ-amu.fr>
2017-12-12 10:11:46 +0100

    s

--- src/code/Demineur.java
+++ src/code/Demineur.java
@@ -1,1 +1,23 @@
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
 
+public class Demineur extends JFrame{
+	Button [] boutons; 
+	
+	public Demineur(int l, int c){
+		setTitle("Démineur");
+		setAlwaysOnTop(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(l*20,c*36);
+		setResizable(false);
+		FlowLayout layout = new FlowLayout();
+ 		setLayout(layout);
+ 		boutons = new Button[l*c];
+ 		for(int i=0; i<l*c; i++ ){
+ 			boutons[i] = new Button(" ");
+ 			add(boutons[i]);
+ 		}
+ 		setVisible(true);
+	}
+}
